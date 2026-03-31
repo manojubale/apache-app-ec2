d463022c17bcef76f96736dc702d2b403393e06f
root <root@ip-172-31-46-115.ap-south-1.compute.internal>
2026-03-31 06:12:23 +0000

    third commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "ap-south-1"
 }
 
-resource "aws_instance" "dummy__ec2" {
+resource "aws_instance" "dummy_ec2" {
 ami = "ami-0931307dcdc2a28c9"
 instance_type = "t3.micro"
 tags = {
