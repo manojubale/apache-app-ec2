60f90d72b2f932915a3466d20c6207498da58f1f
root <root@ip-10-31-0-112.us-east-2.compute.internal>
2026-03-31 07:28:45 +0000

    2nd commit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-2"
 }
 
 resource "aws_instance" "demo_ec2" {
-ami = "ami-0931307dcdc2a28c9"
+ami = "ami-0edc0a81903bf24ef"
 instance_type = "t3.micro"
 tags = {
 Name = "demo-server"
